db5168ae1cc3994c3cdbe96857b42f4ce6c8abfd
yvesdj <yves.dejonghe@student.ap.be>
2022-11-03 07:31:35 +0100

    Added Response handling

--- rentomatic/responses.py
+++ rentomatic/responses.py
@@ -1,6 +1,45 @@
+class ResponseTypes:
+    SUCCESS = "Success"
+    PARAMETERS_ERROR = "ParametersError"
+    RESOURCE_ERROR = "ResourceError"
+    SYSTEM_ERROR = "SystemError"
+
+
+
 class ResponseSuccess:
     def __init__(self, value=None):
+        self.type = ResponseTypes.SUCCESS
         self.value = value
 
     def __bool__(self):
         return True
+
+class ResponseFailure:
+    def __init__(self, type_, message):
+        self.type = type_
+        self.message = message
+
+    @property
+    def value(self):
+        return {"type": self.type, "message": self.message}
+
+    def __bool__(self):
+        return False
+
+    def _format_message(self, msg):
+        if isinstance(msg, Exception):
+            return "{}: {}".format(
+                msg.__class__.__name__, "{}".format(msg)
+            )
+        return msg
+
+
+
+def build_response_from_invalid_request(invalid_request):
+    message = "\n".join(
+        [
+            "{}: {}".format(err["parameter"], err["message"])
+            for err in invalid_request.errors
+        ]
+    )
+    return ResponseFailure(ResponseTypes.PARAMETERS_ERROR, message)
--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,4 +1,55 @@
-from rentomatic.responses import ResponseSuccess
+from rentomatic.responses import (
+    ResponseSuccess,
+    ResponseFailure,
+    ResponseTypes,
+    build_response_from_invalid_request
+    )
+from rentomatic.requests.room_list import RoomListInvalidRequest
+
+SUCCESS_VALUE = {"key": ["value1", "value2"]}
+GENERIC_RESPONSE_TYPE = "Response"
+GENERIC_RESPONSE_MESSAGE = "This is a response"
+
 
 def test_response_succes_is_true():
-    assert bool(ResponseSuccess()) is True
+    response = ResponseSuccess(SUCCESS_VALUE)
+
+    assert bool(response) is True
+
+def test_response_failure_is_false():
+    response = ResponseFailure(
+        GENERIC_RESPONSE_TYPE, GENERIC_RESPONSE_MESSAGE
+    )
+
+    assert bool(response) is False
+
+def test_response_success_has_type_and_message():
+    response = ResponseSuccess(SUCCESS_VALUE)
+
+    assert response.type == ResponseTypes.SUCCESS
+    assert response.value == SUCCESS_VALUE
+
+def test_response_failure_has_type_and_message():
+    response = ResponseFailure(GENERIC_RESPONSE_TYPE, GENERIC_RESPONSE_MESSAGE)
+
+    assert response.type == GENERIC_RESPONSE_TYPE
+    assert response.message == GENERIC_RESPONSE_MESSAGE
+    assert response.value == {
+        "type": GENERIC_RESPONSE_TYPE,
+        "message": GENERIC_RESPONSE_MESSAGE
+    }
+
+def test_response_failure_initialisation_with_exception():
+    response = ResponseFailure(GENERIC_RESPONSE_TYPE, Exception("Just an error message"))
+
+    assert bool(response) == False
+    assert response.type == GENERIC_RESPONSE_TYPE
+    assert response.message == "Exception: Just an error message"
+
+def test_response_failure_from_empty_invalid_request():
+    response = build_response_from_invalid_request(
+        RoomListInvalidRequest()
+    )
+
+    assert bool(response) == False
+    assert response.type == ResponseTypes.PARAMETERS_ERROR
